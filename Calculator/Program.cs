068ea587acddf14f6445ea4b7089fbc4043f5178
VeronikaBelkina <135700425+VeronikaBelkina@users.noreply.github.com>
2024-04-08 13:22:39 +0300

    Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Calculator
+{
+	static void Main()
+	{
+
+		Console.WriteLine("Введите первое число:");
+		double num1 = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Введите операцию (+, -, *, /):");
+		char operation = Convert.ToChar(Console.ReadLine());
+		Console.WriteLine("Введите второе число:");
+		double num2 = Convert.ToDouble(Console.ReadLine());
+
+		double result = 0;
+
+		if (operation == '+')
+		{
+			result = num1 + num2;
+
+			Console.WriteLine("Результат: " + result);
+		}
+		else if (operation == '-')
+		{
+			result = num1 - num2;
+
+			Console.WriteLine("Результат: " + result);
+		}
+		else if (operation == '*')
+		{
+			result = num1 * num2;
+
+			Console.WriteLine("Результат: " + result);
+		}
+		else if (operation == '/')
+		{
+			if (num2 == 0)
+			{
+				Console.WriteLine("Ошибка! Делить на ноль нельзя!");
+			}
+			else
+			{
+				result = num1 / num2;
+
+				Console.WriteLine("Результат: " + result);
+			}
+		}
+
+
+	}
+}
